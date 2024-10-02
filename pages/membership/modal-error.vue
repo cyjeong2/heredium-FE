69c227789386ad4b2e41de0ccd7f33fd0e73b9d6
Nga.Nguyen (Nagi.Nguyen) /Sotatek/SCF <nga.nguyen@sotatek.com>
2024-10-02 12:50:31 +0700

    feat: ih-13 add component popup registration membership error

---
+++ pages/membership/modal-error.vue
@@ -0,0 +1,43 @@
+<template>
+  <KeepAlive>
+    <div class="container">
+      <section class="title">
+        <UButton @click="setOpenModal">Click to show modal error</UButton>
+        <UDialogModalError :is-show="modal1" :message-error="msgError" :is-registered="false">
+          <template #modal-btn2>
+            <UButton class="feedback-btn" @click="modal1 = false">확인</UButton>
+          </template>
+        </UDialogModalError>
+      </section>
+    </div>
+  </KeepAlive>
+</template>
+
+<script>
+
+import UButton from '~/components/user/common/UButton';
+import UDialogModalError from '~/components/user/modal/UDialogModalError.vue';
+
+export default {
+  name: 'ModalErrorPage',
+  components: { UButton, UDialogModalError },
+  data() {
+    return {
+      modal1: false,
+      msgError: 'Message Error Here'
+    }
+  },
+  methods: {
+    setOpenModal() {
+      this.modal1 = !this.modal1
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 500px;
+}
+
+</style>
